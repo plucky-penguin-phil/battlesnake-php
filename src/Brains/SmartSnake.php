8d9703e61c845b440cc6fe8f53157acd970c526f
PluckyPenguinPhil <phil@pluckypenguin.com>
2023-10-27 10:10:54 +0100

    create brain class and set as current brain

---
+++ src/Brains/SmartSnake.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Pluckypenguinphil\Battlesnake\Brains;
+
+use Pluckypenguinphil\Battlesnake\Enums\Direction;
+use Pluckypenguinphil\Battlesnake\Interfaces\IBrain;
+
+class SmartSnake implements IBrain
+{
+
+    public function think(): Direction
+    {
+        // TODO: Implement think() method.
+    }
+}
